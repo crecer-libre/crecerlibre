fbd8a48826a3f23903000f5825282b39de64c464
Francisco Uribe <furibe@vtsystems.cl>
2023-05-24 01:23:24 -0400

    validaciones en forms

--- front/src/components/Admin/Pages/Admin/components/CreateProfessional.jsx
+++ front/src/components/Admin/Pages/Admin/components/CreateProfessional.jsx
@@ -1,65 +1,92 @@
-
-
-
-import React from 'react'
+import { useState } from 'react'
 import { useForm } from '../../../../../hooks/useForm';
 import { createProfessionalAPI } from '../../../../../helpers/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const CreateProfessional = () => {
-    const [value, handleInputChange] = useForm({
-        name: '',
-        username: '',
-        password: '',
-        position: '',
-      });
-    
-      const {
-        name,
-        username,
-        password,
-        position,
-        gender } = value;
-    
-      const handleCreateProfessional = (e) => {
-        e.preventDefault();
-        
-        const professional = {
-          name,
-          username,
-          password,
-          position,
-          role: 'PROFESSIONAL_ROLE',
-          gender
-        };
-    
+  const [value, handleInputChange] = useForm({
+    name: '',
+    username: '',
+    password: '',
+    position: '',
+  });
 
-        createProfessionalAPI(professional)
-          .then((p) => {
-            console.log(p);
-            
-          })
-      };
-    return (
-        <div className='operation'>
-            <h3 className='title-operation'>crear profesionales</h3>
-            <hr />
+  const navigate = useNavigate();
 
-            <div className='operation-create-list'>
-                <div className='operation-create'>
-                    <form>
-                        <input type="text" value={name} name="name" onChange={handleInputChange} placeholder='Nombre profesional' />
-                        <input type="text" value={username} name="username" onChange={handleInputChange} placeholder='Nombre de usuario' />
-                        <input type="text" value={password} name="password" onChange={handleInputChange} placeholder='Contraseña' />
-                        <input type="text" value={position} name="position" onChange={handleInputChange} placeholder='Cargo' />
-                        <select id="gender" value={gender} name="gender" onChange={handleInputChange}>
-                            <option value="N/A">Seleccionar genero</option>
-                            <option value="F">Femenino</option>
-                            <option value="M">Masculino</option>
-                        </select>
-                        <button type="submit" onClick={handleCreateProfessional}>Crear profesional</button>
-                    </form>
-                </div>
-            </div>
+  const [error, setError] = useState('');
+  const [errorCreate, setErrorCreate] = useState('');
+  const [successCreate, setSuccessCreate] = useState('');
+
+  const {
+    name,
+    username,
+    password,
+    position,
+    gender } = value;
+
+  const handleCreateProfessional = (e) => {
+    e.preventDefault();
+
+    const professional = {
+      name,
+      username,
+      password,
+      position,
+      role: 'PROFESSIONAL_ROLE',
+      gender
+    };
+
+    if (name.trim() === '' || username.trim() === '' || password.trim() === '' || position.trim() === '' || gender.trim() === '') {
+      setError('Todos los campos son obligatorios.');
+      return;
+    } else {
+      createProfessionalAPI(professional)
+        .then((p) => {
+          console.log(p);
+
+          if (p.status === 201){
+            setSuccessCreate('El profesional fue creado correctamente.');
+            setTimeout(() => {
+              navigate('./profesionales');
+            }, 2000);
+          } else {
+            setErrorCreate('Error al crear profesional, comuniquese con el administrador.');
+          }
+
+        })
+    }
+
+  };
+  return (
+    <div className='operation'>
+      <h3 className='title-operation'>crear profesionales</h3>
+      <hr />
+
+      <div className='operation-create-list'>
+        <div className='operation-create'>
+          <form>
+            <input type="text" required value={name} name="name" onChange={handleInputChange} placeholder='Nombre profesional' />
+            <input type="text" required value={username} name="username" onChange={handleInputChange} placeholder='Nombre de usuario' />
+            <input type="text" required value={password} name="password" onChange={handleInputChange} placeholder='Contraseña' />
+            <input type="text" required value={position} name="position" onChange={handleInputChange} placeholder='Cargo' />
+            <select id="gender" value={gender} name="gender" onChange={handleInputChange}>
+              <option value="">Seleccionar genero</option>
+              <option value="F">Femenino</option>
+              <option value="M">Masculino</option>
+            </select>
+            {
+              (error !== '') && <p className='alert-form-error animate__animated animate__bounceIn'>{error}</p>
+            }
+            {
+              (errorCreate !== '') && <p className='alert-form-error animate__animated animate__bounceIn'>{errorCreate}</p>
+            }
+            {
+              (successCreate !== '') && <p className='alert-form-success animate__animated animate__bounceIn'>{successCreate}</p>
+            }
+            <button type="submit" onClick={handleCreateProfessional}>Crear profesional</button>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- front/src/components/Admin/Pages/Admin/components/CreateStudent.jsx
+++ front/src/components/Admin/Pages/Admin/components/CreateStudent.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import { useState } from 'react'
 import { useForm } from '../../../../../hooks/useForm';
 import { registerStudentAPI } from '../../../../../helpers/students';
+import { useNavigate } from 'react-router-dom';
 
 export const CreateStudent = () => {
     const [value, handleInputChange] = useForm({
@@ -9,10 +10,16 @@ export const CreateStudent = () => {
         phone: ''
     });
 
+    const navigate = useNavigate();
+
+    const [error, setError] = useState('');
+    const [errorCreate, setErrorCreate] = useState('');
+    const [successCreate, setSuccessCreate] = useState('');
+
     const {
         rut,
         name,
-        phone} = value;
+        phone } = value;
 
     const handleCreateStudent = (e) => {
         e.preventDefault();
@@ -23,10 +30,27 @@ export const CreateStudent = () => {
             phone,
         };
 
-        registerStudentAPI(student)
-            .then((s) => {
-                console.log(s);
-            })
+        if (rut.trim() === '' || name.trim() === '' || phone.trim() === '') {
+            setError('Todos los campos son obligatorios.');
+            return;
+        } else {
+            registerStudentAPI(student)
+                .then((s) => {
+                    console.log(s);
+
+                    if (s.status === 200){
+                        setSuccessCreate('Estudiante creado correctamente.');
+                        setTimeout(() => {
+                          navigate('./estudiantes');
+                        }, 2000);
+                      } else {
+                        setErrorCreate('Error, comuniquese con el administrador.');
+                      }
+
+                })
+
+        }
+
     };
     return (
         <div className='operation'>
@@ -39,6 +63,15 @@ export const CreateStudent = () => {
                         <input type="text" value={rut} name="rut" onChange={handleInputChange} placeholder='Rut estudiante' />
                         <input type="text" value={name} name="name" onChange={handleInputChange} placeholder='Nombre de estudiante' />
                         <input type="text" value={phone} name="phone" onChange={handleInputChange} placeholder='Teléfono' />
+                        {
+                            (error !== '') && <p className='alert-form-error animate__animated animate__bounceIn'>{error}</p>
+                        }
+                        {
+                            (errorCreate !== '') && <p className='alert-form-error animate__animated animate__bounceIn'>{errorCreate}</p>
+                        }
+                        {
+                            (successCreate !== '') && <p className='alert-form-success animate__animated animate__bounceIn'>{successCreate}</p>
+                        }
                         <button type="submit" onClick={handleCreateStudent}>Crear estudiante</button>
                     </form>
                 </div>
