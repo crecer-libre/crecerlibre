bf1f2094a34eeee9f37f92675bff812c04c1641f
Francisco Uribe <furibe@vtsystems.cl>
2023-05-25 14:46:10 -0400

    HOUR PAGE

--- front/src/components/Admin/Pages/Hour/HourPage.jsx
+++ front/src/components/Admin/Pages/Hour/HourPage.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
+import "./styles.css";
 
 export const HourPage = () => {
+
+  const [selectedDateTime, setSelectedDateTime] = useState(null);
+
+  const handleDateTimeChange = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setSelectedDateTime(value);
+  };
+
   return (
-    <div>HourPage</div>
+    <div className='container mx-auto hour-page'>
+      <div className='hour-create'>
+        <h3 className='title-operation'>Crear hora</h3>
+        <hr />
+        <form>
+          <p className='format-rut mt-3'>Debes ingresar Fecha y Hora.</p>
+          <input
+            type="datetime-local"
+            id="fechaHora"
+            name="fechaHora"
+            onChange={handleDateTimeChange}
+          />
+          <button>Crear hora</button>
+        </form>
+      </div>
+      <div className='hour-list'>
+      <h3 className='title-operation'>Listar horas</h3>
+        <hr />
+        
+      </div>
+    </div>
   )
 }
