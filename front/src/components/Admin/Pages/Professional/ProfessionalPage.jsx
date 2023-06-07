4c42e8e77bed9e3ca4aff7019e40b2bf95cbc8d5
Francisco Uribe <furibe@vtsystems.cl>
2023-06-07 00:43:46 -0400

    update

--- front/src/components/Admin/Pages/Professional/ProfessionalPage.jsx
+++ front/src/components/Admin/Pages/Professional/ProfessionalPage.jsx
@@ -16,6 +16,7 @@ export const ProfessionalPage = () => {
                 }
             }).catch((error) => {
                 setError(error.msg);
+                setProfessionals([]);
             })
 
         return () => {
